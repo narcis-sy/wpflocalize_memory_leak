d013e7bf9186b6c8d53925c7f369f48b09ab02bd
Narcis Dumitrescu <n.dumitrescu@synergysportstech.com>
2022-01-14 17:54:01 +0200

    - Click on the Add button 10 times.  - Change the language a couple of times  - Click remove button 10 times  - Take memory snapshot  - Look for objects of type with name containing "parentchanged" 	e.g WPFLocalizeExtension.Providers.ParentChangedNotifierHelper+<>c__DisplayClass0_0<String>         WeakReference<XAMLMarkupExtensions.Base.ParentChangedNotifier>  - At least 20-30 instances should still be alive 		ParentChangedNotifierHelper+<>c__DisplayClass0_0<String>   <== this would be the closure generated class 		WeakReference<XAMLMarkupExtensions.Base.ParentChangedNotifier>

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp3.Annotations;
+using WPFLocalizeExtension.Engine;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _list = new ObservableCollection<string>()
+        {
+            "test"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+            
+        }
+
+        public ObservableCollection<string> List
+        {
+            get => _list;
+            set
+            {
+                if (Equals(value, _list)) return;
+                _list = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+             List.Add("test");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Remove_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (List.Any())
+                List.Remove(List.Last());
+        }
+
+        private void Chance_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (LocalizeDictionary.Instance.Culture.TwoLetterISOLanguageName == "en")
+                LocalizeDictionary.Instance.Culture = new CultureInfo("fr-FR");
+            else
+            {
+                LocalizeDictionary.Instance.Culture = new CultureInfo("en-US");
+            }
+        }
+    }
+}

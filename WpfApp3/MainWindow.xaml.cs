c901f032b42c4e8f91dab044e86e479a72950361
Narcis Dumitrescu <n.dumitrescu@synergysportstech.com>
2022-01-17 11:37:58 +0200

    Debug and scenario 1

--- WpfApp3/MainWindow.xaml.cs
+++ WpfApp3/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -53,6 +54,7 @@ namespace WpfApp3
         private void Add_OnClick(object sender, RoutedEventArgs e)
         {
              List.Add("test");
+             Debug.WriteLine("Add");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -65,12 +67,15 @@ namespace WpfApp3
 
         private void Remove_OnClick(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("Remove");
             if (List.Any())
                 List.Remove(List.Last());
         }
 
         private void Chance_OnClick(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("Change");
+
             if (LocalizeDictionary.Instance.Culture.TwoLetterISOLanguageName == "en")
                 LocalizeDictionary.Instance.Culture = new CultureInfo("fr-FR");
             else
@@ -81,8 +86,28 @@ namespace WpfApp3
 
         private void Collect_OnClick(object sender, RoutedEventArgs e)
         {
+            Debug.WriteLine("Collect");
+
             GC.Collect();
             GC.Collect();
+        }
+
+        private void Scenario1(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.InvokeAsync(() => Add_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Add_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Add_OnClick(sender, e));
+
+            Dispatcher.InvokeAsync(() => Remove_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Remove_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Remove_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Remove_OnClick(sender, e));
+
+            Dispatcher.InvokeAsync(() => Chance_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Chance_OnClick(sender, e));
+            Dispatcher.InvokeAsync(() => Chance_OnClick(sender, e));
+
+            Dispatcher.InvokeAsync(() => Collect_OnClick(sender, e));
         }
     }
 }
